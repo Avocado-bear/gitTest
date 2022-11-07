99ef9e08e9091b6ea0ba624375216e84a09bd26e
Avocado_bear <529963563@qq.com>
2022-11-07 16:12:23 +0800

    push commit

--- src/TTT.java
+++ src/TTT.java
@@ -6,6 +6,7 @@ public class TTT {
         System.out.println("hello git4");
         System.out.println("master test1");
         System.out.println("hot-fix test");
+        System.out.println("push test!");
     }
 
 }

b6daecff6ee2b2c2183f704f151366857be6d4bc
Avocado_bear <529963563@qq.com>
2022-11-07 09:44:44 +0800

    3 git commit

--- src/TTT.java
+++ src/TTT.java
@@ -2,6 +2,7 @@ public class TTT {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("hello github");
+        System.out.println("git3 ");
     }
 
 }
